227df807eef504fe6703b8a811233e8602117d6c
Inbound Now <david@inboundnow.com>
2013-08-29 20:41:37 -0700

    updating config.phps

--- templates/half-and-half/config.php
+++ templates/half-and-half/config.php
@@ -1,68 +1,84 @@
 <?php
 /**
-* WordPress Landing Page Config File
 * Template Name:  Half and Half Template
-*
 * @package  WordPress Landing Pages
 * @author 	David Wells
 */
 
-do_action('lp_global_config'); // The lp_global_config function is for global code added by 3rd party extensions
+do_action('lp_global_config'); // global config action hook
 
 //gets template directory name to use as identifier - do not edit - include in all template files
 $key = lp_get_parent_directory(dirname(__FILE__)); 
 
-//adds template data to global array for use with landing page plugin - edit theme category and description only. 
+$lp_data[$key]['info'] = 
+array(
+	'version' => "1.0.1", // Version Number
+	'label' => "Half and Half", // Nice Name
+	'category' => 'Miscellaneous, v1', // Template Category
+	'demo' => 'http://demo.inboundnow.com/go/half-and-half-lander-preview/', // Demo Link
+	'description'  => 'Half and Half is a template with two content areas on each side of the page. One side has your conversion area and the other your content on the page.' // template description
+);
 
-//EDIT - START - defines template information - helps categorizae template and provides additional popup information
-$lp_data[$key]['category'] = "Miscellaneous"; 
-// Add Landing Page to a specific category. 
-$lp_data[$key]['version'] = "1.0.0.1"; 
-// Add version control to your template.
-$lp_data[$key]['description'] = "This template illustrates capabilities of this plugin's templating system.."; 
-// Add description visible to the user
-$lp_data[$key]['features'][] = lp_list_feature("Demo Link","http://demo.inboundnow.com/go/half-and-half-lander-preview/"); 
-// Add a live demo link to illustration the page functionality to the user
-$lp_data[$key]['features'][] = lp_list_feature("Half and Half is a template with two content areas on each side of the page. One side has your conversion area and the other your content on the page."); 
-// Description of the landing page visible to the user.
-$lp_data[$key]['thumbnail'] = LANDINGPAGES_URLPATH.'templates/'.$key.'/thumbnail.png'; 
-//thumbnail
-
-//DO NOT EDIT - adds template to template selection dropdown 
-$lp_data[$key]['value'] = $key; //do not edit this
-$lp_data[$key]['label'] = ucwords(str_replace('-',' ',$key)); //do not edit this
-
-
-//************************************************
-// Add User Options to Your Landing Page Template
-//************************************************
-
-// Add a radio button option to your theme's options panel.	
-$options = array('1' => 'on','0'=>'off');
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"radio","display-social","1","Display Social Media Share Buttons","Toggle social sharing on and off", $options);
-
-// Add a dropdown toggle to the landing page options panel	
-$options = array('right'=>'Conversion Area on right','left'=>'Conversion Area on left');
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"dropdown","sidebar","right","Page Layout","Align Conversion/Form Area to the left or the right", $options);	
-
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","content-color","ffffff","Content Background Color","Use this setting to change the template's main content area color", $options=null);
-
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","content-text-color","000000","Content Text Color","Use this setting to change the content text color", $options=null);	
-
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","sidebar-color","EE6E4C","Conversion Area Background Color","Use this setting to change the template's sidebar color", $options=null);
-
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","submit-button-color","38A6F0","Submit Button Background Color","Use this setting to change the template's submit button color.", $options=null);
-
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","sidebar-text-color","ffffff","Conversion Area Text Color","Use this setting to change the sidebar text color", $options=null);
+// Define Meta Options for template
+// These values are returned in the template's index.php file with lp_get_value($post, $key, 'field-id') function
+$lp_data[$key]['settings'] = 
+array(
+    array(  
+        'label' => 'Display Social Media Share Buttons', // Label of field
+        'description' => "Display Social Media Share Buttons", // field description
+        'id' => 'display-social', // metakey.
+        'type'  => 'radio', // text metafield type
+        'default'  => '1', // default content
+        'options' => array('1' => 'on','0'=>'off'), // options for radio
+        'context'  => 'normal' // Context in screen for organizing options
+        ),
+    array(
+        'label' => "Page Layout",
+        'description' => "Align Conversion/Form Area to the left or the right",
+        'id'  => 'sidebar', 
+        'type'  => 'dropdown',
+        'default'  => 'left',
+        'options' => array('right'=>'Conversion Area on right','left'=>'Conversion Area on left'),
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => 'Content Background Color',
+        'description' => "Content Background Color",
+        'id'  => 'content-color', 
+        'type'  => 'colorpicker',
+        'default'  => 'ffffff',
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => 'Content Text Color',
+        'description' => "Content Text Color",
+        'id'  => 'content-text-color',
+        'type'  => 'colorpicker',
+        'default'  => '000000',
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => 'Conversion Area Background Color',
+        'description' => "Conversion Area Background Color",
+        'id'  => 'sidebar-color', 
+        'type'  => 'colorpicker',
+        'default'  => 'EE6E4C',	
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => 'Submit Button Background Color',
+        'description' => "Submit Button Background Color",
+        'id'  => 'submit-button-color',
+        'type'  => 'colorpicker',
+        'default'  => '38A6F0',
+        'context'  => 'normal'
+        ),
+	array(
+        'label' => 'Conversion Area Text Color',
+        'description' => "Conversion Area Text Color",
+        'id'  => 'sidebar-text-color',
+        'type'  => 'colorpicker',
+        'default'  => 'ffffff',
+        'context'  => 'normal'
+        )
+);	
--- templates/rsvp-envelope/config.php
+++ templates/rsvp-envelope/config.php
@@ -1,62 +1,84 @@
 <?php
 /**
-* WordPress Landing Page Config File
 * Template Name:  RSVP Envelope Template
-*
 * @package  WordPress Landing Pages
 * @author 	David Wells
 */
 
-do_action('lp_global_config'); // The lp_global_config function is for global code added by 3rd party extensions
+do_action('lp_global_config'); // global config action hook
 
 //gets template directory name to use as identifier - do not edit - include in all template files
 $key = lp_get_parent_directory(dirname(__FILE__)); 
 
-//adds template data to global array for use with landing page plugin - edit theme category and description only. 
+$lp_data[$key]['info'] = 
+array(
+	'version' => "1.0.1", // Version Number
+	'label' => "RSVP Template", // Nice Name
+	'category' => 'Miscellaneous, v1', // Template Category
+	'demo' => 'http://demo.inboundnow.com/go/rsvp-envelope-lander-preview/', // Demo Link
+	'description'  => 'This template is great for sending out invitations to events.' // template description
+);
 
-//EDIT - START - defines template information - helps categorizae template and provides additional popup information
-$lp_data[$key]['category'] = "Miscellaneous"; 
-$lp_data[$key]['description'] = "RSVP Envelope Template"; 
-$lp_data[$key]['version'] = "1.0.0.1"; 
-$lp_data[$key]['features'][] = lp_list_feature("Demo Link","http://demo.inboundnow.com/go/rsvp-envelope-lander-preview/"); 
-$lp_data[$key]['features'][] = lp_list_feature("This template is great for sending out invitations to events."); 
-$lp_data[$key]['thumbnail'] = LANDINGPAGES_URLPATH.'templates/'.$key.'/thumbnail.png'; 
-
-
-//DO NOT EDIT - adds template to template selection dropdown 
-$lp_data[$key]['value'] = $key; 
-$lp_data[$key]['label'] = ucwords(str_replace('-',' ',$key)); 
-
-
-//************************************************
-// Add User Options to Your Landing Page Template
-//************************************************
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","body-color","CCCCCC","Template body color","Use this setting to change the template's body background color", $options=null);
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","headline-color","ffffff","Headline Color","Use this setting to change the template's headline text color", $options=null);	
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","text-color","7C7873","Text Color","Use this setting to change the template's text color", $options=null);	
-
-// Add Colorpicker
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"colorpicker","form-text-color","7C7873","Form Text Color","Use this setting to change the template's form text color", $options=null);	
-
-// Radio Button Example
-// Add a radio button option to your theme's options panel.	
-$options = array('1' => 'on','0'=>'off');
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"radio","display-social","1","Display Social Media Share Buttons","Toggle social sharing on and off", $options);
-
-// Add a dropdown toggle to the landing page options panel	
-$options = array('right'=>'Envelope on right', 'left'=>'Envelope on left');
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"dropdown","sidebar","right","Sidebar Layout","Align sidebar to the left or the right", $options);	
-	
-// Media Uploaded Example
-// Add a media uploader field to your landing page options	
-$lp_data[$key]['options'][] = 
-	lp_add_option($key,"media","media-example","","Background Image","Enter an URL or upload an image for the banner.", $options=null);
+// Define Meta Options for template
+// These values are returned in the template's index.php file with lp_get_value($post, $key, 'field-id') function
+$lp_data[$key]['settings'] = 
+array(
+    array(  
+        'label' => "Template body color", // Label of field
+        'description' => "Use this setting to change the template's body background color", // field description
+        'id' => 'body-color', // metakey.
+        'type'  => 'colorpicker', // text metafield type
+        'default'  => 'CCCCCC', // default content
+        'context'  => 'normal' // Context in screen for organizing options
+        ),
+    array(
+        'label' => 'Headline Color',
+        'description' => "Use this setting to change the template's headline text color",
+        'id'  => 'headline-color', 
+        'type'  => 'colorpicker',
+        'default'  => 'ffffff',
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => "Text Color",
+        'description' => "Text Color",
+        'id'  => 'text-color', 
+        'type'  => 'colorpicker',
+        'default'  => '7C7873',
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => "Form Text Color",
+        'description' => "Form Text Color",
+        'id'  => 'form-text-color',
+        'type'  => 'colorpicker',
+        'default'  => '7C7873',
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => "Display Social Media Share Buttons",
+        'description' => "Display Social Media Share Buttons",
+        'id'  => 'display-social', 
+        'type'  => 'radio',
+        'default'  => '1',
+        'options' => array('1' => 'on','0'=>'off'),
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => "Sidebar Layout",
+        'description' => "Align sidebar to the left or the right",
+        'id'  => 'sidebar',
+        'type'  => 'dropdown',
+        'default'  => 'right',
+        'options' => array('right'=>'Envelope on right', 'left'=>'Envelope on left'),
+        'context'  => 'normal'
+        ),
+    array(
+        'label' => 'Background Image',
+        'description' => "Background Image",
+        'id'  => 'media-example',
+        'type'  => 'media',
+        'default'  => '',
+        'context'  => 'normal'
+        )
+    );	
