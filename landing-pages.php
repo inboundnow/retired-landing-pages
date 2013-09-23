b704349fb5d7073f796a2359a8166dfb319dc1d6
Hudson Atwell <hudson.atwell@gmail.com>
2013-09-23 11:10:02 -0500

    Incrementing version number in landing-pages.php

--- landing-pages.php
+++ landing-pages.php
@@ -3,12 +3,12 @@
 Plugin Name: Landing Pages
 Plugin URI: http://www.inboundnow.com/landing-pages/
 Description: The first true all-in-one Landing Page solution for WordPress, including ongoing conversion metrics, a/b split testing, unlimited design options and so much more!
-Version:  1.1.7
+Version:  1.1.8
 Author: David Wells, Hudson Atwell
 Author URI: http://www.inboundnow.com/
 */
 			
-define('LANDINGPAGES_CURRENT_VERSION', '1.1.7' );
+define('LANDINGPAGES_CURRENT_VERSION', '1.1.8' );
 define('LANDINGPAGES_URLPATH', WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 define('LANDINGPAGES_PATH', WP_PLUGIN_DIR.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 define('LANDINGPAGES_PLUGIN_SLUG', 'landing-pages' );
