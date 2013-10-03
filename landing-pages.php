ca230018acab171e97a17e077d8db11cd298873e
Hudson Atwell <hudson.atwell@gmail.com>
2013-10-03 17:58:17 -0500

    Adding Easy Licensing for Extensions:
    EG: /*PREPARE THIS EXTENSION FOR LICESNING*/
    	$license = new LP_EXTENSION_LICENSE(  LP_IMPORT_NAME , plugin_basename( dirname(__FILE__) ) );

--- landing-pages.php
+++ landing-pages.php
@@ -50,6 +50,7 @@ include_once('functions/functions.admin.php');
 include_once('modules/module.global-settings.php');
 include_once('modules/module.clone.php');
 include_once('modules/module.extension-updater.php');
+include_once('modules/module.extension-licensing.php');
 include_once('modules/module.install.php');
 include_once('modules/module.alert.php');
 }
