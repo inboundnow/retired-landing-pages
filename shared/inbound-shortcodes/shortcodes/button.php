e1d632be085d823536fa9cbf86ebf72b558b617e
Inbound Now <david@inboundnow.com>
2013-10-22 11:14:22 -0700

    update short codes

--- shared/inbound-shortcodes/shortcodes/button.php
+++ shared/inbound-shortcodes/shortcodes/button.php
@@ -12,7 +12,19 @@
  * 	----------------------------------------------------- */
 	$shortcodes_config['button'] = array(
 		'no_preview' => false,
+		
 		'options' => array(
+			'style' => array(
+				'name' => __('Button Style', INBOUND_LABEL),
+				'desc' => __('Select the button style.', INBOUND_LABEL),
+				'type' => 'select',
+				'options' => array(
+					'default' => 'Default',
+					'flat' => 'flat',
+					'sunk' => 'sunk'
+				),
+				'std' => 'default'
+			),
 			'content' => array(
 				'name' => __('Label', INBOUND_LABEL),
 				'desc' => __('Enter the button text label.', INBOUND_LABEL),
@@ -29,6 +41,13 @@
 					'large' => 'Large'
 				),
 				'std' => 'normal'
+			),
+			'width' => array(
+				'name' => __('Custom Width', INBOUND_LABEL),
+				'desc' => __('Enter in pixel width or % width. Example: 200 <u>or</u> 100%', INBOUND_LABEL),
+				'type' => 'text',
+				'std' => '',
+				'class' => 'main-design-settings',
 			),
 			'color' => array(
 				'name' => __('Button Color', INBOUND_LABEL),
@@ -64,14 +83,14 @@
 				'std' => ''
 			),
 			'blank' => array(
-				'name' => __('Link Targeting', INBOUND_LABEL),
+				'name' => __('Open Link in New Tab?', INBOUND_LABEL),
 				'checkbox_text' => __('Check to open the link in the new tab.', INBOUND_LABEL),
 				'desc' => '',
 				'type' => 'checkbox',
 				'std' => '1'
 			),
 		),
-		'shortcode' => '[button size="{{size}}" color="{{color}}" icon="{{icon}}" url="{{url}}" blank="{{blank}}"]{{content}}[/button]',
+		'shortcode' => '[button style="{{style}}" size="{{size}}" color="{{color}}" icon="{{icon}}" url="{{url}}" blank="{{blank}}"]{{content}}[/button]',
 		'popup_title' => __('Insert Button Shortcode', INBOUND_LABEL)
 	);
 
@@ -81,6 +100,7 @@
 
 	function fresh_shortcode_button( $atts, $content = null ) {
 		extract(shortcode_atts(array(
+			'style'=> '',
 			'size' => '',
 			'color' => '',
 			'icon' => '',
@@ -89,8 +109,34 @@
 		), $atts));
 
 		$class = "button $color $size";
-		$icon = '<i class="icon-'. $icon .'"></i>&nbsp;';
+		$icon_raw = 'icon-'. $icon;
 		$target = ($blank) ? ' target="_blank"' : '';
+		$button_start = "";
 
-		return '<a class="'. $class .'" href="'. $url .'"'. $target .'>'. $icon . $content .'</a>';
+			switch( $style ) {
+
+					case 'default':
+						$button  = $button_start;
+						$button .= '<a class="'. $class .'" href="'. $url .'"'. $target .'><i class="'.$icon_raw.'"></i>&nbsp;' . $content .'</a>';
+						$button .= $button_start;
+						break;
+
+					case 'flat' :
+						$button  = $button_start;
+						$button .= '<a href="'. $url .'"'. $target .' class="inbound-flat-btn facebook"><span class="'.$icon_raw.' icon"></span><span>'.$content.'</span></a>';
+					
+						$button .= $button_start;
+						break;
+					case 'sunk' :
+						$button  = $button_start;
+						$button .= '<div class="inbound-sunk-button-wrapper">
+									<a href="'. $url .'"'. $target .' class="inbound-sunk-button inbound-sunk-light"><span class="'.$icon_raw.' icon"></span>'.$content.'</a>
+									</div>';
+					
+						$button .= $button_start;
+						break;	
+				}
+			
+
+		return $button;
 	}
