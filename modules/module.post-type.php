65cba226720c946d1afd177672ae110eefe4bc12
Inbound Now <david@inboundnow.com>
2014-01-21 18:59:27 -0800

    update file for local host

--- modules/module.post-type.php
+++ modules/module.post-type.php
@@ -306,6 +306,9 @@ if (is_admin())
 			$datetime = the_modified_date('YmjH',null,null,false);
 			$permalink = $permalink = $permalink.'?dt='.$datetime;
 			$thumbnail = 'http://s.wordpress.com/mshots/v1/' . urlencode(esc_url($permalink)) . '?w=140';
+			if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+			    $thumbnail = LANDINGPAGES_URLPATH . '/images/localhost.png';
+			}
 			echo "<a title='".__('Click to Preview this variation' , LANDINGPAGES_TEXT_DOMAIN) ."' class='thickbox' href='".$permalink."?lp-variation-id=0&iframe_window=on&post_id=".$post->ID."&TB_iframe=true&width=640&height=703' target='_blank'><img src=".$thumbnail."' style='width:150px;height:110px;' title='Click to Preview'></a>";
 
 		}
