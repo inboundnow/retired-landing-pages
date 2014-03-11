e2a4b66b940a5bcad6e55cd11f183353c5af7d3e
JasonHo <jasonho.ib@gmail.com>
2014-03-11 17:21:45 +0700

    Another test case, #92

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -19,11 +19,11 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
   /**
    * Ensure that the plugin has been installed and activated.
    */
-  //function test_plugin_activated() {
-  //
-  //  $this->assertTrue( is_plugin_active( 'plugin-slug/main-plugin-file.php' ) );
+  function test_plugin_activated() {
 
-  //}
+    $this->assertTrue( is_plugin_active( 'plugin-slug/main-plugin-file.php' ) );
+
+  }
 
 }
 
