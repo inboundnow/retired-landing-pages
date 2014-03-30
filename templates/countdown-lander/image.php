ca4e4de3234c45d9b7b884dffc8c2801490668c3
Inbound Now <david@inboundnow.com>
2014-03-30 15:51:48 -0700

    misc bug fixes

--- templates/countdown-lander/image.php
+++ templates/countdown-lander/image.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // file: image.php
 // Dynamically Create a clear png for css background opacities
 header("Content-type: image/png");
@@ -8,7 +8,7 @@ $hex_value = $_GET['hex'];
 function HexToRGB($hex) {
         $hex = preg_replace("/#/", "", $hex);
         $color = array();
- 
+
         if(strlen($hex) == 3) {
             $color['r'] = hexdec(substr($hex, 0, 1) . $r);
             $color['g'] = hexdec(substr($hex, 1, 1) . $g);
@@ -19,28 +19,28 @@ function HexToRGB($hex) {
             $color['g'] = hexdec(substr($hex, 2, 2));
             $color['b'] = hexdec(substr($hex, 4, 2));
         }
- 
+
         return $color;
-        
+
 }
 
 $RBG_array = HexToRGB($hex_value);
-$red = $RBG_array [r];
-$green = $RBG_array [g];
-$blue = $RBG_array [b];
- 
-// Set the image 
-$img = imagecreatetruecolor(10,10); // 10 x 10 px
-imagesavealpha($img, true); 
+$red = (isset($RBG_array['r'])) ? $RBG_array['r'] : '0';
+$green = (isset($RBG_array['g'])) ? $RBG_array['g'] : '0';
+$blue = (isset($RBG_array['b'])) ? $RBG_array['b'] : '0';
 
-// Fill the image with transparent color 
+// Set the image
+$img = imagecreatetruecolor(10,10); // 10 x 10 px
+imagesavealpha($img, true);
+
+// Fill the image with transparent color
 $color = imagecolorallocatealpha($img,$red,$green,$blue,50); // 50 is the opacity
-imagefill($img, 0, 0, $color); 
+imagefill($img, 0, 0, $color);
 
 // Return the image
-imagepng($img); 
+imagepng($img);
 
-// Destroy image 
+// Destroy image
 imagedestroy($img);
 
 // usage in html: <image src="path-to-file/image.php?hex=HEXCOLOR">
