dfd47fd95e3ccef35127391b1c88467e132101ba
SkepticalHippo <edvinas@fluxministry.com>
2014-06-03 01:01:19 +0100

    Issue 121 Fix
    https://github.com/inboundnow/landing-pages/issues/121

--- modules/module.metaboxes.php
+++ modules/module.metaboxes.php
@@ -34,10 +34,12 @@ function lp_thumbnail_metabox() {
 
 	if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
 
-		if (file_exists(LANDINGPAGES_UPLOADS_URLPATH . 'templates/' . $template . '/thumbnail.png'))
+		if (file_exists(LANDINGPAGES_UPLOADS_URLPATH . 'templates/' . $template . '/thumbnail.png')) {
 			$thumbnail = LANDINGPAGES_UPLOADS_URLPATH . 'templates/' . $template . '/thumbnail.png';
-		else
+		}
+		else {
 			$thumbnail = LANDINGPAGES_URLPATH . 'templates/' . $template . '/thumbnail.png';
+		}
 
 	} else {
 		$thumbnail = 'http://s.wordpress.com/mshots/v1/' . urlencode(esc_url($permalink)) . '?w=250';
