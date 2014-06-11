3bd0e79671d165698aa046c5af5f69ce497961b3
Hudson Atwell <hudson.atwell@gmail.com>
2014-06-11 17:29:23 -0500

    Adding class.shortcodes.email-template.php to shared

--- landing-pages.php
+++ landing-pages.php
@@ -95,30 +95,34 @@ function inbound_load_shared_lp_init(){
 add_action( 'plugins_loaded', 'inbound_load_shared_landing_pages' , 11 );
 function inbound_load_shared_landing_pages(){
 
-  /* Check if Shared Files Already Loaded */
-  if (defined('INBOUDNOW_SHARED'))
-    return;
+	/* Check if Shared Files Already Loaded */
+	if (defined('INBOUDNOW_SHARED')) {
+		return;
+	}
 
-  /* Define Shared Constant for Load Prevention*/
-  define('INBOUDNOW_SHARED','loaded');
+	/* Define Shared Constant for Load Prevention*/
+	define('INBOUDNOW_SHARED','loaded');
 
-  /* Singleton Shared Class Loads */
-  include_once('shared/shortcodes/inbound-shortcodes.php');  // Shared Shortcodes
-  include_once('shared/classes/class.menu.php');  // Inbound Marketing Menu
-  include_once('shared/classes/class.feedback.php');  // Inbound Feedback Form
-  include_once('shared/classes/class.debug.php');  // Inbound Debug & Scripts Class
-  include_once('shared/classes/class.compatibility.php');  // Inbound Compatibility Class
-  include_once('shared/classes/class.form.php');  // Mirrored forms
-  include_once('shared/tracking/store.lead.php'); // Lead Storage from landing pages
+	/* Singleton Shared Class Loads */
+	include_once('shared/shortcodes/inbound-shortcodes.php');  // Shared Shortcodes
+	include_once('shared/classes/class.menu.php');  // Inbound Marketing Menu
+	include_once('shared/classes/class.feedback.php');  // Inbound Feedback Form
+	include_once('shared/classes/class.debug.php');  // Inbound Debug & Scripts Class
+	include_once('shared/classes/class.compatibility.php');  // Inbound Compatibility Class
+	include_once('shared/classes/class.form.php');  // Mirrored forms
 
-  include_once('shared/extend/inboundnow.extend.php');
-  include_once('shared/extend/inboundnow.extension-licensing.php'); // Legacy - Inboundnow Package Licensing
-  include_once('shared/extend/inboundnow.extension-updating.php'); // Legacy -Inboundnow Package Updating
-  include_once('shared/extend/inboundnow.global-settings.php'); // Inboundnow Global Settings
-  include_once('shared/assets/assets.loader.class.php');  // Load Shared CSS and JS Assets
-  include_once('shared/functions/global.shared.functions.php'); // Global Shared Utility functions
-  include_once('shared/functions/global.leads.cpt.php'); // Shared Lead functionality
-  include_once('shared/metaboxes/template.metaboxes.php');  // Shared Shortcodes
+	include_once('shared/classes/class.templating-engine.php');  // Inbound Templating Engine Class
+	include_once('shared/classes/class.shortcodes.email-template.php'); //Inbound Email Templating Shortcodes Class
+	include_once('shared/tracking/store.lead.php'); // Lead Storage from landing pages
+
+	include_once('shared/extend/inboundnow.extend.php');
+	include_once('shared/extend/inboundnow.extension-licensing.php'); // Legacy - Inboundnow Package Licensing
+	include_once('shared/extend/inboundnow.extension-updating.php'); // Legacy -Inboundnow Package Updating
+	include_once('shared/extend/inboundnow.global-settings.php'); // Inboundnow Global Settings
+	include_once('shared/assets/assets.loader.class.php');  // Load Shared CSS and JS Assets
+	include_once('shared/functions/global.shared.functions.php'); // Global Shared Utility functions
+	include_once('shared/functions/global.leads.cpt.php'); // Shared Lead functionality
+	include_once('shared/metaboxes/template.metaboxes.php');  // Shared Shortcodes
 
 }
 
