010e16872d267beabc2032018a7a52693016a3a1
Hudson Atwell <hudson.atwell@gmail.com>
2014-07-16 15:00:32 -0500

    Fix for lead email notifications and akismet filtering

--- landing-pages.php
+++ landing-pages.php
@@ -3,7 +3,7 @@
 Plugin Name: Landing Pages
 Plugin URI: http://www.inboundnow.com/landing-pages/
 Description: The first true all-in-one Landing Page solution for WordPress, including ongoing conversion metrics, a/b split testing, unlimited design options and so much more!
-Version:	1.6.1
+Version:	1.6.2
 Author: Inbound Now
 Author URI: http://www.inboundnow.com/
 Text Domain: landing-pages
@@ -38,7 +38,7 @@ if (!class_exists('Inbound_Landing_Pages_Plugin')) {
 		*/
 		private static function load_constants() {		
 						
-			define('LANDINGPAGES_CURRENT_VERSION', '1.6.1' );
+			define('LANDINGPAGES_CURRENT_VERSION', '1.6.2' );
 			define('LANDINGPAGES_URLPATH', WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 			define('LANDINGPAGES_PATH', WP_PLUGIN_DIR.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 			define('LANDINGPAGES_PLUGIN_SLUG', plugin_basename( dirname(__FILE__) ) );
