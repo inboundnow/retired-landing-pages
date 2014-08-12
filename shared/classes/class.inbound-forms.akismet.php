09802dab1d063638d5b9dc252b350e24b877b818
Jeff Brand <jeff@deltafactory.com>
2014-08-12 13:58:14 -0400

    Remove additional anonymous PHP function.

--- shared/classes/class.inbound-forms.akismet.php
+++ shared/classes/class.inbound-forms.akismet.php
@@ -175,7 +175,8 @@ if ( !class_exists('Inbound_Akismet') ) {
 	}
 
 	/* Load Email Templates Post Type Pre Init */
-	add_action('plugins_loaded' , function() {
+	function inbound_akismet_init() {
 		$GLOBALS['Inbound_Akismet'] = new Inbound_Akismet();
-	} );
+	}
+	add_action('plugins_loaded' , 'inbound_akismet_init' );
 }
