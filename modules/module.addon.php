33fab3143784f84a1ffd889182a50f5db65cf529
Inbound Now <david@inboundnow.com>
2014-09-28 11:58:35 -0700

    remove store iframe

--- modules/module.addon.php
+++ modules/module.addon.php
@@ -5,9 +5,9 @@ function lp_addon_display()
 	?>
 	<script type='text/javascript'>
 		jQuery(document).ready(function($) {
-
+			/*
 			new easyXDM.Socket({
-				remote: "http://plugin.inboundnow.com/downloads/category/extensions/",  
+				remote: "http://plugin.inboundnow.com/downloads/category/extensions/",
 				container: document.getElementById("lp-store-iframe-container"),
 				onMessage: function(message, origin){
 					var height = Number(message) + 1000;
@@ -15,10 +15,12 @@ function lp_addon_display()
 					this.container.getElementsByTagName("iframe")[0].style.height = height + "px";
 				}
 			});
-		
+
 		});
+		*/
 	</script>
-	<div id="lp-store-iframe-container">
+	<div id="zlp-store-iframe-container">
+		The store is temporarily disabled. Please visit the <a href="http://www.inboundnow.com/market">Inbound Now markertplace</a>
     </div>
 
 <?php
