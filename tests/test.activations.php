d468a9e9f1a53f704f2ee66823859538a4a1121c
Hudson Atwell <atwell.publishing@gmail.com>
2014-09-29 18:09:50 -0500

    I don't think I can deactivate a plugin in a unit test.

--- tests/test.activations.php
+++ tests/test.activations.php
@@ -28,63 +28,6 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 		$this->assertTrue( is_plugin_active( 'leads/wordpress-leads.php' ) );
 	}
 	
-	/**
-	* Ensure calls to action plugin is active
-	*/
-	function test_calls_to_action_activated() {
-		$this->assertTrue( is_plugin_active( 'cta/wordpress-cta.php' ) );
-	}
-	
-	/**
-	* Deactivate Calls to Action
-	*/
-	function test_deactivate_calls_to_action() {
-
-		deactivate_plugins( 'cta/wordpress-cta.php' );
-		
-		if (!is_plugin_active( 'cta/wordpress-cta.php' ) ) {
-			$this->assertTrue( true );
-		} else {
-			var_dump(is_plugin_active( 'cta/wordpress-cta.php' ));
-			$this->assertTrue( false );
-		}	
-		
-	}
-	
-	/**
-	* Deactivate Leads
-	*/
-	function test_deactivate_leads() {
-		deactivate_plugins( 'leads/wordpress-leads.php' );
-		
-		if (!is_plugin_active( 'leads/wordpress-leads.php' ) ) {
-			$this->assertTrue( true );
-		} else {
-			$this->assertTrue( false );
-		}	
-	}
-	
-	/**
-	 *  Reactivate Leads
-	 */
-	function text_reactivate_leads() {
-		if (!activate_plugin( 'leads/wordpress-leads.php')) {
-			$this->assertTrue( true );
-		} else {
-			$this->assertTrue( false );
-		}
-	}
-	
-	/**
-	 *  Reactivate Calls to Action
-	 */
-	function text_reactivate_cta() {
-		if (!activate_plugin( 'cta/wordpress-cta.php')) {
-			$this->assertTrue( true );
-		} else {
-			$this->assertTrue( false );
-		}
-	}
 }
 
 ?>
