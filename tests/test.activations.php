210e9efb302a0508532eb95e02890bd45f720f27
Hudson Atwell <atwell.publishing@gmail.com>
2014-09-29 17:49:32 -0500

    testing filters in unit tests.

--- tests/test.activations.php
+++ tests/test.activations.php
@@ -39,14 +39,28 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 	* Deactivate Calls to Action
 	*/
 	function test_deactivate_calls_to_action() {
-		$this->assertTrue( deactivate_plugins( 'cta/wordpress-cta.php' ) );
+		deactivate_plugins( 'cta/wordpress-cta.php' );
+		
+		if (!is_plugin_active( 'cta/wordpress-wordpress.php' ) ) {
+			$this->assertTrue( true );
+		} else {
+			$this->assertTrue( false );
+		}
+		
 	}
 	
 	/**
 	* Deactivate Leads
 	*/
 	function test_deactivate_leads() {
-		$this->assertTrue( deactivate_plugins( 'leads/wordpress-leads.php' ) );
+		deactivate_plugins( 'leads/wordpress-leads.php' );
+		
+		if (!is_plugin_active( 'leads/wordpress-leads.php' ) ) {
+			$this->assertTrue( true );
+		} else {
+			$this->assertTrue( false );
+		}
+		
 	}
 }
 
