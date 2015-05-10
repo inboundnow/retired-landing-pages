15405ba5ffc707a4a64ef00f3631535c6ea48821
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-10 16:07:47 -0500

    synching shared

--- shared/classes/class.feedback.php
+++ shared/classes/class.feedback.php
@@ -1,10 +1,6 @@
 <?php
 /* Inbound Now Menu Class */
-// - Ajax call
-// - get data
-// - callback /api?params
-// - return json
-// - store transient
+
 if (!class_exists('Inbound_Feedback')) {
 	class Inbound_Feedback {
 	static $add_feedback;
@@ -92,54 +88,6 @@ if (!class_exists('Inbound_Feedback')) {
 			@curl_close($ch2);
 			echo $response2;
 
-		}
-	}
-
-
-	public static function feedback_callback() {
-		$domain = "$_SERVER[HTTP_HOST]";
-		$lead_count = self::get_count('wp-lead');
-		$lp_count = self::get_count('landing-pages');
-		$cta_count = self::get_count('cta');
-		//echo $domain; exit;
-		$url = "http://localhost:8080/api/test"; // localhost
-
-		$response = wp_remote_post( $url, array(
-					'method' => 'POST',
-					'timeout' => 45,
-					'redirection' => 5,
-					'httpversion' => '1.0',
-					'blocking' => true,
-					'headers' => array(),
-					'body' => array( 'site' => $domain,
-									 'cta' => $cta_count,
-									 'lp' => $lp_count,
-									 'leads' => $lp_count )
-				    )
-				);
-
-		if ( is_wp_error( $response ) ) {
-		   $error_message = $response->get_error_message();
-		   echo "Something went wrong: $error_message";
-		} else {
-		   //echo 'Response:<pre>';
-		   $json = $response['body'];
-		   //print_r( $response['body'] );
-		   $array = json_decode($json, true);
-		   //print_r($array);
-		   if(isset($array['error'])) {
-		   		echo $array['error']; exit;
-		   }
-
-		  if(isset($array['handshake'])) {
-		  	$valid = $array['handshake'];
-		  }
-
-		  // set transient here
-
-		   //print_r( $response );
-		   //echo '</pre>';
-		   //exit;
 		}
 	}
 	public static function get_count($type) {
@@ -394,23 +342,21 @@ box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(102,175,233,0.6);}
  </style>
 	<script type="text/javascript">
 	jQuery(document).ready(function($) {
-			jQuery("body").on('click', '#inbound-automation-footer', function () {
+					jQuery("body").on('click', '#inbound-automation-footer', function () {
 
-				jQuery("#lp-slide-toggle").slideToggle();
-				jQuery("#lp-open-close").toggleClass("lp-options-up");
-				jQuery("#footer").toggleClass("lp-options-on");
-			});
+					jQuery("#lp-slide-toggle").slideToggle();
+					jQuery("#lp-open-close").toggleClass("lp-options-up");
 
-			jQuery("body").on('click', '.inbound-close-fb', function () {
-				jQuery("#lp-slide-toggle").slideToggle();
-			});
-
-			jQuery("body").on('submit', '#inbound-feedback', function (e) {
-				e.preventDefault(); // halt normal form
-				var feedback = jQuery('#inbound-feedback-message').val();
-				var email = jQuery('#inbound-feedback-email-field').val();
-
-				if (typeof (feedback) != "undefined" && feedback != null && feedback != "") {
+					jQuery("#footer").toggleClass("lp-options-on");
+					});
+					jQuery("body").on('click', '.inbound-close-fb', function () {
+					jQuery("#lp-slide-toggle").slideToggle();
+						});
+					jQuery("body").on('submit', '#inbound-feedback', function (e) {
+					e.preventDefault(); // halt normal form
+					var feedback = jQuery('#inbound-feedback-message').val();
+					var email = jQuery('#inbound-feedback-email-field').val();
+					if (typeof (feedback) != "undefined" && feedback != null && feedback != "") {
 						jQuery.ajax({
 						type: 'POST',
 						url: ajaxurl,
@@ -432,10 +378,10 @@ box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(102,175,233,0.6);}
 
 							}
 						});
-				} else {
-					$("#lp-slide-toggle textarea").css('border', 'red');
-				}
-			});
+						} else {
+						$("#lp-slide-toggle textarea").css('border', 'red');
+						}
+					});
 	});
 
 	</script>
