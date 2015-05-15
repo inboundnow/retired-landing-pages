be8d62816e28417f614dd73efc4148f8a71dea02
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-15 18:40:47 -0500

    update shared

---
+++ classes/class.statistics.php
@@ -0,0 +1,16 @@
+<?php
+
+class Landing_Pages_Statistics {
+
+    /**
+     * Gets impression count for all landing page variations
+     * @param $id
+     */
+
+    public static function get_impressions( $id ) {
+
+    }
+
+
+}
+
