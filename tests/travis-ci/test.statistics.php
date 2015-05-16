7912e047cfaa7c7778e572ccd47a9f71267aef26
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-16 18:05:22 -0500

    danger

--- tests/travis-ci/test.statistics.php
+++ tests/travis-ci/test.statistics.php
@@ -57,7 +57,7 @@ class Tests_Statistics extends WP_UnitTestCase {
         /* includes */
         include_once LANDINGPAGES_PATH . 'classes/class.statistics.php';
 
-        $permalink = get_permalink( $this->lp_id );
+        $permalink = get_post_permalink( $this->lp_id );
         echo $permalink;
         $response = wp_remote_get( $permalink );
         $response = wp_remote_get( $permalink );
