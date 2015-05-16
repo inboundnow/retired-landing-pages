093d0a7c7232cd0241cc733e53d70e2d8b445cd4
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-15 22:08:24 -0500

    unit tests

--- tests/travis-ci/test.statistics.php
+++ tests/travis-ci/test.statistics.php
@@ -19,7 +19,7 @@ class Tests_Statistics extends WP_UnitTestCase {
      */
     function test_create_dummy_landing_page() {
         /* load the class used to create the dummy landing page */
-        include_once LANDINGPAGES_PATH . 'modules/module.install.php';
+        include_once LANDINGPAGES_PATH . 'modules/module.install.php'; 
 
         /* create a landing page */
         $this->assertTrue( inbound_create_default_post_type() );
