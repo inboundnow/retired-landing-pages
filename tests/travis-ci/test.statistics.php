6e23a7305928f93020b289ee5496be9e743dc6ee
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-16 13:06:05 -0500

    test to see if get_post works

--- tests/travis-ci/test.statistics.php
+++ tests/travis-ci/test.statistics.php
@@ -8,6 +8,29 @@
 class Tests_Statistics extends WP_UnitTestCase {
 
     /**
+     * Test to see if get_post works.
+     *
+     * Compares a post ID ($org_post_id) with post ID
+     * taken out of get_post ($new_post_id).
+     * If they don't match, get_post() doesn't work, and it will
+     * return an error.
+     */
+    function test_get_post() {
+        //Create new post using method provided by WP
+        $org_post_id = $this->factory->post->create();
+
+        //get post object using the new post's ID
+        $post_obj = get_post( $org_post_id );
+
+        //Get the post ID as given to us by get_post
+        $new_post_id = $post_obj->ID;
+
+        //Use pre-defined method to test if the two ID's match
+        $this->assertEquals( $org_post_id, $new_post_id );
+
+    }
+    
+    /**
      * creates a dummy landing page for testing
      */
     function test_create_demo_lander() {
