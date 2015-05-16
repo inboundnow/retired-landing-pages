16256426f2cb85705fcd18d26afd738aaf36e536
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-16 12:11:14 -0500

    unit tests

--- tests/travis-ci/test.statistics.php
+++ tests/travis-ci/test.statistics.php
@@ -13,20 +13,14 @@ class Tests_Statistics extends WP_UnitTestCase {
     function setUp() {
         /* load the class used to create the dummy landing page */
         include_once LANDINGPAGES_PATH . 'modules/module.install.php';
-
-        $option_name = "lp_settings_general";
-        $option_key = "default_landing_page";
-        $current_user = wp_get_current_user();
-        add_option( $option_name, '' );
-
-        //update_user_meta( get_current_user_id(), 'tgmpa_dismissed_notice', 0 ); // Clean dismiss settings
-        //delete_option( 'lp_settings_general' );
-        $lp_default_options = get_option($option_name);
-
-        print_r($lp_default_options);
-        /* create a landing page */
-        $this->lp_id =  inbound_create_default_post_type();
+        self::get_demo_landing_page();
         echo 'here' . $this->lp_id;
+    }
+
+
+    function get_demo_landing_page() {
+        $options = get_option("lp_settings_general");
+        $this->lp_id = $options['default_landing_page'];
     }
 
 
@@ -45,15 +39,7 @@ class Tests_Statistics extends WP_UnitTestCase {
     function test_reset_landing_page_stats() {
 
         $landing_pages = get_posts( array(
-            'post_type'=> 'landing-page',
-            'post_status' => 'draft'
-        ));
-
-        print_r($landing_pages);
-
-        $landing_pages = get_posts( array(
-            'post_type'=> 'post',
-            'post_status' => 'draft'
+            'ID'=> $this->lp_id;
         ));
 
         print_r($landing_pages);
