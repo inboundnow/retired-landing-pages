7438ae15170bf8e20074738b586840ca315bd6bb
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-23 20:46:27 -0500

    no message

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -6,7 +6,8 @@
  */
 
 /* load wp */
-require  ABSPATH . '/wp-load.php';
+require '../../../wp-load.php';
+require '../../../wp-admin/includes/plugin.php';
 
 /* needed for testcase.php */
 define( 'WP_TESTS_FORCE_KNOWN_BUGS', false );
