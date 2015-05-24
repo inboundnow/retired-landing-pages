42c0d9ee38580fe61f57e5108f761b512eb4157a
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-23 19:43:18 -0500

    no message

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -7,29 +7,6 @@
  * @package wordpress-plugin-tests
  */
 
-// Activates this plugin in WordPress so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-	  'active_plugins' => array(
-		'landing-pages/landing-pages.php',
-		'cta/wordpress-cta.php',
-		'leads/wordpress-leads.php',
-	  ),
-);
 
-
-/**
-*  Load WordPress's testing environment boostrap.php. This is not shipped with the git repo but is unloaded into the travis build unless WP_DEVELOP_DIR is set with a custom locatin.
-*/
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
-}
- 
-/**
-*  WordPress Dev Environment rebuilds the datbase on bootstrap.
-*/
-update_option( 'siteurl' , 'http://inboundsoon.dev' );
-update_option( 'home' , 'http://inboundsoon.dev' );
-
-flush_rules();
+/* include testcase.php from WordPress's tests repo (on travis build ) */
+require '../../../../tests/phpunit/includes/testcase.php';
