266a497e297926d5b231092d52ac100864309c4f
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-23 19:53:50 -0500

    no message

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -7,6 +7,8 @@
  * @package wordpress-plugin-tests
  */
 
+/* needed for testcase.php */
+define( 'WP_TESTS_FORCE_KNOWN_BUGS', false );
 
 /* include testcase.php from WordPress's tests repo (on travis build ) */
 require '../../../../tests/phpunit/includes/testcase.php';
