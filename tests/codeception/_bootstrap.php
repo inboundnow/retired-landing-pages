3cb19edf07762447b2a9a773757e3e2a04297667
Hudson Atwell <hudson.atwell@gmail.com>
2015-07-07 19:41:07 -0500

    no message

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -1,15 +1,12 @@
 <?php
 
-/* load coception addons */
-require '../../../vendor/lucatume/wp-browser/autoload.php';
 
 /* load wp */
 require '../../../wp-load.php';
 require '../../../wp-admin/includes/plugin.php';
 
-/* load required landing pages files */
-include_once LANDINGPAGES_PATH . 'modules/module.install.php';
-include_once LANDINGPAGES_PATH . 'classes/class.statistics.php';
+/* load coception addons */
+require  LANDINGPAGES_PATH 'vendor/lucatume/wp-browser/autoload.php';
 
 /* Set current users */
 wp_set_current_user( 1 );
