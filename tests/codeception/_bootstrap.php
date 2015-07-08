7a4db376623ae9f2616eec26044fe65010b2808e
Hudson Atwell <hudson.atwell@gmail.com>
2015-07-07 20:05:16 -0500

    no message

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -6,7 +6,7 @@ require '../../../wp-load.php';
 require '../../../wp-admin/includes/plugin.php';
 
 /* load coception addons */
-require  LANDINGPAGES_PATH 'vendor/lucatume/wp-browser/autoload.php';
+require  LANDINGPAGES_PATH . 'vendor/lucatume/wp-browser/autoload.php';
 
 /* Set current users */
 wp_set_current_user( 1 );
