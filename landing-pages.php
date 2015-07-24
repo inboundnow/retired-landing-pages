13049e82a747d143b71409c1205124cbd536dd78
Hudson Atwell <hudson.atwell@gmail.com>
2015-07-24 15:15:24 -0500

    removing very old legacy extension licensing class: https://github.com/inboundnow/landing-pages/issues/186

--- landing-pages.php
+++ landing-pages.php
@@ -73,7 +73,6 @@ if (!class_exists('Inbound_Landing_Pages_Plugin')) {
                     include_once( LANDINGPAGES_PATH . 'classes/class.cloning.php');
 					include_once( LANDINGPAGES_PATH . 'modules/module.global-settings.php');
 					include_once( LANDINGPAGES_PATH . 'modules/module.extension-updater.php');
-					include_once( LANDINGPAGES_PATH . 'modules/module.extension-licensing.php');
 					include_once( LANDINGPAGES_PATH . 'modules/module.welcome.php');
 					include_once( LANDINGPAGES_PATH . 'modules/module.install.php');
 					include_once( LANDINGPAGES_PATH . 'modules/module.landing-page.php');
