5a1165bed44b504023add055691a0284913f6249
Giulio Daprela <giulio.daprela@gmail.com>
2015-09-14 14:12:57 +0700

    Triumph Added skeleton folders and files for the new template https://github.com/inboundnow/landing-pages/issues/248

---
+++ templates/triumph/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* Template Name: Acacia
+* @package  WordPress Landing Pages
+* @author   Inbound Template Generator
+*/
+
+/* Declare Template Key */
+$key = basename(dirname(__FILE__));
+$path = LANDINGPAGES_UPLOADS_URLPATH ."$key/";
+$url = plugins_url();
+
+
+/* Include ACF Field Definitions  */
+include_once(LANDINGPAGES_PATH.'templates/'.$key.'/config.php');
+
+/* Enqueue Styles and Scripts */
+function acacia_enqueue_scripts() {
+	//wp_enqueue_script( 'jquery' );
+	//wp_enqueue_script( 'acacia-bootstrap-js', plugins_url('assets/js/bootstrap.min.js', __FILE__),'','', true );
+	
+	//wp_enqueue_style( 'acacia-bootstrap-css', plugins_url('assets/css/bootstrap.css', __FILE__) );
+}
+
+add_action('wp_enqueue_scripts', 'acacia_enqueue_scripts');
+
+/* Define Landing Pages's custom pre-load hook for 3rd party plugin integration */
+do_action('wp_head');
+
+if (have_posts()) : while (have_posts()) : the_post();
+
+$post_id = get_the_ID();
+?>
+
+<!DOCTYPE html>
+
+
+</html>
+<?php
+
+endwhile; endif;
